66e6eddb43da6533b88acd8842b68f868bbd80be
Anastasia Pankina <siapankina@gmail.com>
2020-01-13 21:34:02 +0000

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Stock = () => (
+const Stock = ({ stock, onClick }) => (
   <div>
-
     <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+      <div className="card-body" onClick={() => onClick(stock)}>
+        <h5 className="card-title">{stock.name}</h5>
+        <p className="card-text">{stock.ticker + ":" + stock.price}</p>
       </div>
     </div>
-
-
   </div>
 );
 
-export default Stock
+export default Stock;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import React, { Component } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "../components/SearchBar";
 
 class MainContainer extends Component {
-
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+          handleRadio={this.props.handleRadio}
+          sortBy={this.props.sortBy}
+          handleSelect={this.props.handleSelect}
+          selectSort={this.props.selectSort}
+        />
 
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
+        <div className="row">
+          <div className="col-8">
+            <StockContainer
+              filteredStocks={this.props.filteredStocks}
+              // stocks={this.props.stocks}
+              onClick={this.props.onClick}
+            />
           </div>
+          <div className="col-4">
+            <PortfolioContainer
+              myStocks={this.props.myStocks}
+              removeStock={this.props.removeFromMyStocks}
+            />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
