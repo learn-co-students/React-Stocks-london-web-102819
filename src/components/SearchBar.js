f885d9878c64a9a9acd3b04313b1fc871690fcf6
Qing <chingwang.qw@gmail.com>
2020-01-13 21:04:10 +0000

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = () => {
+const SearchBar = props => {
+  const { handleSortByChange, sortBy, filterBy, handleFilterByChange } = props;
   return (
     <div>
-
       <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+      <label for="alphabetically">
+        <input
+          id="alphabetically"
+          type="radio"
+          value="alphabetically"
+          checked={sortBy === "alphabetically"}
+          onChange={handleSortByChange}
+        />
         Alphabetically
       </label>
+      &nbsp;||&nbsp;
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input
+          type="radio"
+          value="price"
+          checked={sortBy === "price"}
+          onChange={handleSortByChange}
+        />
         Price
       </label>
-      <br/>
-
+      <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
+        <select onChange={handleFilterByChange}>
+          <option
+            value="Tech"
+            selected={filterBy === "Tech" ? "selected" : undefined}
+          >
+            Tech
+          </option>
+          <option
+            value="Sportswear"
+            selected={filterBy === "Sportswear" ? "selected" : undefined}
+          >
+            Sportswear
+          </option>
+          <option
+            value="Finance"
+            selected={filterBy === "Finance" ? "selected" : undefined}
+          >
+            Finance
+          </option>
         </select>
       </label>
-
-
     </div>
   );
-}
-
+};
 
 export default SearchBar;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,31 +1,111 @@
-import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import React, { Component } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "../components/SearchBar";
+
+const STOCKS_URL = "http://localhost:3000/stocks";
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    stockInProfolio: [],
+    sortBy: "alphabetically",
+    filterBy: "Finance"
+  };
+
+  componentDidMount() {
+    fetch(STOCKS_URL)
+      .then(res => res.json())
+      .then(stocks => this.setState({ stocks }));
+  }
+
+  // checkStock = stockId => {
+  //   const stocks = [...this.state.stocks];
+  // };
+
+  // checkStock = stockId => this.state.stockInProfolio.includes(stockId);
+
+  handleSortByChange = event => {
+    this.setState({
+      sortBy: event.target.value
+    });
+  };
+
+  handleFilterByChange = event => {
+    this.setState({
+      filterBy: event.target.value
+    });
+  };
+
+  stocksToDisplay = () => {
+    const stocks = [...this.state.stocks].filter(
+      stock => stock.type === this.state.filterBy
+    );
+
+    if (this.state.sortBy === "alphabetically") {
+      stocks.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    }
+
+    if (this.state.sortBy === "price") {
+      stocks.sort((a, b) => {
+        return a.price - b.price;
+      });
+    }
+
+    return stocks;
+  };
+
+  handleBuyStock = stock => {
+    const stocks = [...this.state.stockInProfolio];
+    if (stocks.includes(stock)) {
+      return window.alert("you've aleready bought this stock");
+    }
+
+    this.setState({
+      stockInProfolio: [...this.state.stockInProfolio, stock]
+    });
+  };
+
+  handleSaleStock = stock => {
+    const stocks = [...this.state.stockInProfolio];
+    const stockToSale = stocks.filter(s => s.id !== stock.id);
+
+    this.setState({
+      stockInProfolio: stockToSale
+    });
+  };
 
   render() {
+    const { stocks, stockInProfolio, sortBy, filterBy } = this.state;
     return (
       <div>
-        <SearchBar/>
-
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
+        <SearchBar
+          sortBy={sortBy}
+          handleSortByChange={this.handleSortByChange}
+          filterBy={filterBy}
+          handleFilterByChange={this.handleFilterByChange}
+        />
+        <div className="row">
+          <div className="col-8">
+            <StockContainer
+              stocksToRender={this.stocksToDisplay()}
+              handleBuyStock={this.handleBuyStock}
+            />
           </div>
+          <div className="col-4">
+            <PortfolioContainer
+              stockInProfolio={stockInProfolio}
+              checkStock={this.checkStock}
+              stocks={stocks}
+              onSaleStock={this.handleSaleStock}
+            />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
